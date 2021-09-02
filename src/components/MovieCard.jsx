8cb5eaf8227608270457604738e7fbf5fc42942e
User <gabrielkleber006@gmail.com>
2021-09-02 09:21:07 -0300

    requisitos 3,4,5.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movieInfo } = this.props;
+    return (
+      <section>
+        <img src={ movieInfo.imagePath } alt="" />
+        <h3>{ movieInfo.title }</h3>
+        <h4>{movieInfo.subtitle}</h4>
+        <p>{movieInfo.storyline }</p>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {movies.map((all) => <MovieCard key={ all.title } movieInfo={ all } />)}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
